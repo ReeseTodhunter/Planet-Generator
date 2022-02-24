8d1121f8dbf75749fecca909d400ffb785958f94
ReeseTodhunter <reeset538@gmail.com>
2022-02-24 18:04:33 +0000

    Beginning to add physics

---
+++ Solar Sim/Assets/Scripts/Physics/Physics.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour
+{
+    [SerializeField]
+    private float mass;
+
+    [SerializeField]
+    private float rotationalSpeed;
+
+    [SerializeField]
+    private float movementForce;
+
+    [SerializeField]
+    private float distanceFromPlanet;
+}
